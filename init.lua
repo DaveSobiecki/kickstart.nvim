8981fba13acf5d64951e2d1d6cb991c025275188
dawid.sobiecki <dawid.sobiecki@dbschenker.com>
2025-03-20 09:35:04 +0100

    Added harpoon plugin

--- init.lua
+++ init.lua
@@ -399,6 +399,12 @@ require('lazy').setup({
     opts = {},
   },
 
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+  },
+
   --  { -- code runner
   --  'CRAG666/code_runner.nvim',
   --  config = true,
@@ -1215,6 +1221,59 @@ require('nvim-tree').setup {
     end,
   },
 }
+
+--HARPOON CONFIG
+local harpoon = require 'harpoon'
+harpoon:setup()
+
+--Extension
+local harpoon_extensions = require 'harpoon.extensions'
+harpoon:extend(harpoon_extensions.builtins.highlight_current_file())
+
+-- basic telescope configuration
+local conf = require('telescope.config').values
+local function toggle_telescope(harpoon_files)
+  local file_paths = {}
+  for _, item in ipairs(harpoon_files.items) do
+    table.insert(file_paths, item.value)
+  end
+
+  require('telescope.pickers')
+    .new({}, {
+      prompt_title = 'Harpoon',
+      finder = require('telescope.finders').new_table {
+        results = file_paths,
+      },
+      previewer = conf.file_previewer {},
+      sorter = conf.generic_sorter {},
+    })
+    :find()
+end
+
+vim.keymap.set('n', '<Leader>e', function()
+  toggle_telescope(harpoon:list())
+end, { desc = 'Open harpoon window' })
+
+vim.keymap.set('n', '<leader>a', function()
+  harpoon:list():add()
+end, { desc = 'Add a file to harpoon' })
+vim.keymap.set('n', '<C-e>', function()
+  harpoon.ui:toggle_quick_menu(harpoon:list())
+end)
+
+--vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+--vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+--vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+--vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set('n', '<C-S-P>', function()
+  harpoon:list():prev()
+end)
+vim.keymap.set('n', '<C-S-N>', function()
+  harpoon:list():next()
+end)
+--END HARPOON CONFIG
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
